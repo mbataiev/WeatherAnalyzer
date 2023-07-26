1478a4785b81bfc6f9ebf3a54427aeb61255fda3
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-07-26 18:16:30 +0300

    Added Weather Service module

---
+++ weather-service/src/main/java/com/app/weather/WeatherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherServiceApplication.class, args);
+    }
+
+}

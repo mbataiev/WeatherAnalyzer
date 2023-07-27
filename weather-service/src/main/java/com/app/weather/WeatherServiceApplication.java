f69da8b9addf2b08d6a297902dedd0e55f7bb66b
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-07-27 19:01:48 +0300

    added swagger documentation

--- weather-service/src/main/java/com/app/weather/WeatherServiceApplication.java
+++ weather-service/src/main/java/com/app/weather/WeatherServiceApplication.java
@@ -1,9 +1,25 @@
 package com.app.weather;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Weather REST API Documentation",
+                description = "Weather REST API Documentation",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Maksym",
+                        email = "max.bataiev@gmail.com",
+                        url = "https://github.com/mbataiev"
+                )
+        )
+)
 public class WeatherServiceApplication {
 
     public static void main(String[] args) {
--- weather-service/src/main/java/com/app/weather/controller/WeatherController.java
+++ weather-service/src/main/java/com/app/weather/controller/WeatherController.java
@@ -2,12 +2,17 @@ package com.app.weather.controller;
 
 import com.app.weather.dto.WeatherDataDto;
 import com.app.weather.service.WeatherService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/weather")
+@Tag(
+        name = "Produce weather data",
+        description = "Collect and produce weather data from http://api.openweathermap.org"
+)
 public class WeatherController {
 
     private final WeatherService weatherService;

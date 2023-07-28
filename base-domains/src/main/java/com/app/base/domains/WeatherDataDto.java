abf87a46178c218b678f2c76fd226c7936d7d8ba
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-07-28 18:33:55 +0300

    Added base-domains module

---
+++ base-domains/src/main/java/com/app/base/domains/WeatherDataDto.java
@@ -0,0 +1,25 @@
+package com.app.base.domains;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WeatherDataDto {
+    private Long id;
+    @NotEmpty(message = "Weather can not be empty!")
+    private String weather;
+    @NotEmpty(message = "Temperature can not be empty!")
+    private Double temperature;
+    @NotEmpty(message = "Wind speed can not be empty!")
+    private Double windSpeed;
+    @NotEmpty(message = "Visibility can not be empty!")
+    private Double visibility;
+    @NotEmpty(message = "Created date can not be empty!")
+    private LocalDateTime createdDate;
+}
---
+++ base-domains/src/main/java/com/app/base/domains/WeatherEvent.java
@@ -0,0 +1,4 @@
+package com.app.base.domains;
+
+public class WeatherEvent {
+}

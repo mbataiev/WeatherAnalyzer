8394df89c8a8abaaf3009982fd1c262f0b350051
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-07-28 14:20:07 +0300

    typo

--- weather-service/src/main/java/com/app/weather/WeatherServiceApplication.java
+++ weather-service/src/main/java/com/app/weather/WeatherServiceApplication.java
@@ -1,6 +1,5 @@
 package com.app.weather;
 
-import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;

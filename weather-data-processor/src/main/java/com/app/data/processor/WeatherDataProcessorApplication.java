a71a91b8a7406005bef11b5b146771322b4de067
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-07-29 14:28:33 +0300

    added processor module

---
+++ weather-data-processor/src/main/java/com/app/data/processor/WeatherDataProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.app.data.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDataProcessorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherDataProcessorApplication.class, args);
+    }
+
+}

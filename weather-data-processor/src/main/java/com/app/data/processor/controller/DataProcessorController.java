b20a606133358c8fed4f4b40325b51535979c406
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-07-30 17:32:56 +0300

    added docs

--- weather-data-processor/src/main/java/com/app/data/processor/controller/DataProcessorController.java
+++ weather-data-processor/src/main/java/com/app/data/processor/controller/DataProcessorController.java
@@ -2,12 +2,17 @@ package com.app.data.processor.controller;
 
 import com.app.data.processor.domains.WeatherDataDto;
 import com.app.data.processor.service.DataProcessorService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/process")
 @AllArgsConstructor
+@Tag(
+        name = "Process weather data",
+        description = "Receive weather json from Weather Service, processes it and passes on"
+)
 public class DataProcessorController {
 
     private DataProcessorService service;

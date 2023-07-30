967ef695433e3a4145c8c294c7c2669fd71d85c1
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-07-30 20:40:07 +0300

    added data storage module

---
+++ weather-data-storage/src/main/java/com/app/data/storage/WeatherDataStorageApplication.java
@@ -0,0 +1,13 @@
+package com.app.data.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDataStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherDataStorageApplication.class, args);
+	}
+
+}

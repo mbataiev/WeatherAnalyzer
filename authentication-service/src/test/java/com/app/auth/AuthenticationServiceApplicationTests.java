cbf866ab6f9f45b4c297faece67d36513c7dec96
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-08-01 20:24:44 +0300

    added auth module

---
+++ authentication-service/src/test/java/com/app/auth/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

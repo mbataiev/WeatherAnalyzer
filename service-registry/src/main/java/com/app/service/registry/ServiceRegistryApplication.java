3dc1331d245f8c7c8110060896322afe09f9299f
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-08-02 18:51:06 +0300

    renamed

--- service-registry/src/main/java/com/app/service/registry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/app/service/registry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package net.javaguides.serviceregistry;
+package com.app.service.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- service-registry/src/test/java/com/app/service/registry/ServiceRegistryApplicationTests.java
+++ service-registry/src/test/java/com/app/service/registry/ServiceRegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package net.javaguides.serviceregistry;
+package com.app.service.registry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

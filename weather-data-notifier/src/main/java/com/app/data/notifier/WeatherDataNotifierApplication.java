a2b336acac572698d7dca3858d15b460be0395dd
superlalkaTV <maksim.bataev.2016@gmail.com>
2023-08-03 13:34:25 +0300

    added notifier module

---
+++ weather-data-notifier/src/main/java/com/app/data/notifier/WeatherDataNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.app.data.notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDataNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherDataNotifierApplication.class, args);
+	}
+
+}
---
+++ weather-data-notifier/src/test/java/com/app/data/notifier/WeatherDataNotifyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.data.notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDataNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
